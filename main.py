0cc46d4401092907fdd06d8bc499be37b7a28ee5
wagha-builds <arwaghmode09@gmail.com>
2025-09-08 16:50:35 +0530

    Add description, image and links.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ with col2:
 
 st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 # If the data in the csv file were to be separated with a "," instead of ";", they would've been automatically separated
 # without calling the sep parameter.
@@ -26,7 +26,13 @@ df = pd.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
