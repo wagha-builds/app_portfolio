d63b8e55ea72cdd5cdfe76485ac2241b2563a891
wagha-builds <arwaghmode09@gmail.com>
2025-09-10 19:29:11 +0530

    Add "Contact Us" page.

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photos.jpeg")
 
 with col2:
     st.title("Atharva Waghmode")
